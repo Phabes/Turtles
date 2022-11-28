94b97e710339153a7236995d7f4cd38babe375e2
Phabes <quinlanvos1234@gmail.com>
2022-11-28 19:54:47 +0100

    Initial Commit

---
+++ src/main/java/Kasztany/Turtles/TurtlesApplication.java
@@ -0,0 +1,12 @@
+package Kasztany.Turtles;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TurtlesApplication {
+	public static void main(String[] args) {
+		Application.launch(App.class, args);
+	}
+}
---
+++ src/test/java/Kasztany/Turtles/TurtlesApplicationTests.java
@@ -0,0 +1,13 @@
+package Kasztany.Turtles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
